11ff18196d7189449693c89de16c15eb91f494e5
Lu Wang <wangl33@uw.edu>
2020-02-23 00:39:06 -0800

    lu-branch modify panel1 and add voiceinput

--- panel1.py
+++ panel1.py
@@ -1,7 +1,8 @@
 import os.path
 import csv
-from PyQt5 import uic
+from PyQt5 import uic, QtCore
 from PyQt5.QtWidgets import QApplication, QWidget, QComboBox, QPushButton, QListWidget, QListWidgetItem
+import speech_recognition as sr
 
 Form, Base = uic.loadUiType("panel1.ui")
 
@@ -18,7 +19,7 @@ class Panel1:
         self.dc = dataContainer
         self.item_metadata = {}
         self.ordered_item_metadata = []
-        self.current_selected_item_to_add = None        
+        self.current_selected_item_to_add = None
 
     def getWidget(self):
         return self.widget
@@ -33,6 +34,12 @@ class Panel1:
 
         self.addButton = self.widget.findChild(QPushButton, "addButton")
         self.addButton.clicked.connect(self.addButtonClicked)
+
+        self.reduceButton = self.widget.findChild(QPushButton, "reduceButton")
+        self.reduceButton.clicked.connect(self.reduceButtonClicked)
+
+        self.voiceButton = self.widget.findChild(QPushButton, "voiceButton")
+        self.voiceButton.clicked.connect(self.voiceButtonClicked)
 
         self.shoppingListWidget = self.widget.findChild(QListWidget, "shoppingItems")
 
@@ -68,3 +75,37 @@ class Panel1:
         self.dc.add_to_shopping_list(item_to_add)
 
         self.updateShoppingListUI()
+
+    def reduceButtonClicked(self):
+        if self.current_selected_item_to_add is None:
+            return
+
+        item_to_reduce = self.ordered_item_metadata[self.current_selected_item_to_add]
+
+        self.dc.reduce_to_shopping_list(item_to_reduce)
+
+        self.updateShoppingListUI()
+
+    def voiceButtonClicked(self):
+        # textChanged = QtCore.pyqtSignal(str)
+
+        r = sr.Recognizer()
+        m = sr.Microphone()
+
+        # while True:
+        print("Say somethig!")
+        with m as source:
+            audio = r.listen(source)
+            print("Got it! Now to recognize it...")
+            try:
+                value = r.recognize_google(audio)
+                # self.textChanged.emit(value)
+                print("You said: {}".format(value))
+                if self.current_selected_item_to_add is None:
+                    return
+                # need to modify
+                item_to_add = self.ordered_item_metadata[self.current_selected_item_to_add]
+                self.dc.add_to_shopping_list(item_to_add)
+                self.updateShoppingListUI()
+            except sr.UnknownValueError:
+                print("Oops")
